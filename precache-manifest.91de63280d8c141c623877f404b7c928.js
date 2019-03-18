c4f6bce751de2bcaf0f52b2f0c31c0f86e6236cf
Roberto Achar <robertoachar@gmail.com>
2019-03-18 20:53:19 -0300

    Updates

---
+++ precache-manifest.91de63280d8c141c623877f404b7c928.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "acd4079d3dc1cdf9c860",
+    "url": "/react-axios-cancel-token/static/js/main.50adaa1a.chunk.js"
+  },
+  {
+    "revision": "8d2e6138fc7d633de072",
+    "url": "/react-axios-cancel-token/static/js/runtime~main.7dc97699.js"
+  },
+  {
+    "revision": "1dcf3fb98283735452a5",
+    "url": "/react-axios-cancel-token/static/js/2.cadad293.chunk.js"
+  },
+  {
+    "revision": "e860a35667b84cc685f183fdf2dd8af7",
+    "url": "/react-axios-cancel-token/index.html"
+  }
+];
